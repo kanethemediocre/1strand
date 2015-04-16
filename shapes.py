ad274c024b5ccecaf44975fb88634da2888c3cf8
kanethemediocre <kanethemediocre@gmail.com>
2015-04-16 17:08:17 -0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -103,7 +103,43 @@ def inputpath(xxpath, yypath, zzpath):
    return float(raw_input("Height?"))
       
          
-      
+def loadfile(xxpath, yypath, zzpath):
+   print "This function can load a previously saved 2d shape"
+   filename1 = open(raw_input("Filename?"), "r")
+   Height = float(raw_input("Height?"))
+   quitflag = 0
+   i=0
+   garbagex1 = filename1.readline() #First two movements
+   garbagey1 = filename1.readline() #are origin and lift
+   garbagez1 = filename1.readline() #which are already added 
+   garbagezzz = filename1.readline() #to the path in the main
+   garbagex1 = filename1.readline() #program.  We need to discard
+   garbagey1 = filename1.readline() #them or they will get duplicated
+   garbagez1 = filename1.readline() #by the loading process
+   garbagezzz = filename1.readline()
+   while quitflag == 0:
+      line1 = filename1.readline()
+      i=i+1 #This is the line number on the file.
+      if line1 == "":
+         quitflag=1
+         print "End of file reached"
+         print "Read", str(i), "lines.\n"
+      else:
+         if ( i % 4 ) == 1:  #This where the file really gets parsed.           
+            xxpath.append(float(line1)) #if i did this all in one
+         if ( i % 4 ) == 2:             #loop execution i'd have
+            yypath.append(float(line1)) #end of file being passed as 
+         if ( i % 4 ) == 3:             #a float and shitting more bricks
+            zzpath.append(float(line1)) #if it ends unexpectedly.
+         if ( i % 4 ) == 0:
+            if line1 != "zzz\n":
+               print "file flag error at line", str(i)
+   filename1.close()
+   Height = float(raw_input("Height?"))
+   return Height       
+            
+            
+            
          
       
    
